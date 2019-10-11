262749bd4538e90c0897401a7f43f17d204906f4
Keenan Johns <keenanjj13@protonmail.com>
2019-10-11 12:33:51 -0500

    Testing communication between servers

--- client/App.js
+++ client/App.js
@@ -1,13 +1,31 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Style from './App.css';
+
+const axios = require('axios');
 
 class App extends Component {
     constructor(props) {
         super(props);
-        this.state = {}
+        this.state = {
+            test: ''
+        }
+    }
+
+    componentDidMount() {
+        axios.get('http://localhost:3002/test').then(({ data }) => {
+            this.setState({ test: data });
+        }).catch(() => {
+            console.log(`Error with GET request at 'localhost:3002/test'`);
+        })
     }
 
     render() {
-        return (<div>Hello Keenan!</div>);
+        return (
+            <div>
+                <h1>Hello Keenan! This is the client from the proxy!</h1>
+                <h2>{this.state.test}</h2>
+            </div>
+        );
     }
 }
 
